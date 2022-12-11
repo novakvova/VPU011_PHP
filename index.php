7c932cb26b4ce6e29ab78e3d4f520dede681f935
novakvova <novakvova@gmail>
2022-12-11 10:17:02 +0200

    One page php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Головна сторінка</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/site.css">
+</head>
+<body>
+<?php include($_SERVER["DOCUMENT_ROOT"].'/_header.php'); ?>
+
+
+<h1 class="text-center">Список товарів</h1>
+
+
+
+<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
